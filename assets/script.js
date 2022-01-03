1ded92240d9a2fd74f511d8680e5b8517a38ab16
Tyler Everett <tylertechnotrade@gmail.com>
2022-01-02 18:14:21 -0800

    working on js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const apiKey="3f899b32efb4f563be09ef8dd895a9e0";
+const searchColEl = $('#search-column');
+const cityInputEl = $('#city-input');
+const searchBtnEl = $('#search-btn');
+const cityBtnsEl = $('#city-btns');
+const cityContainer = $('#city-container');
+const forecastContainerEl = $('#forecast-container');
