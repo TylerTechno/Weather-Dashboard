0981c1269fa0ed24316a1a900aeb2f0540d25e05
Tyler Everett <tylertechnotrade@gmail.com>
2022-01-02 18:40:59 -0800

    working on js

--- assets/script.js
+++ assets/script.js
@@ -62,3 +62,82 @@ class User {
           return requestUrl;
         }
     }
+
+    class CurrentWeather {
+        constructor(data, name = 'default') {
+          this.name = name;
+          this.date = data.current.dt;
+          this.icon = data.current.weather[0].icon;
+          this.temp = data.current.temp;
+          this.wind = data.current.wind_speed;
+          this.humidity = data.current.humidity;
+          this.uvi = data.current.uvi;
+          this.uvi_color = this.getUviColor(this.uvi);
+        }
+        fillCityCurrentContainerInfo() {
+            let date = moment(this.date * 1000).format('MM/DD/YY');
+            let icon = `http://openweathermap.org/img/wn/${this.icon}@4x.png`
+            let currentDiv = $('<div>').addClass('current-card-1 card m-2 p-2 border border-1 border-dark');
+    let currentDate = $('<h3>').addClass('current-date').text(`${this.name} ${date}`);
+    let currentIcon = $('<img>').addClass('current-weather-icon').attr({'src': icon, 'alt': 'Weather Icon', 'width': '10%'});
+    let currentTemp = $('<p>').addClass('current-temp').text(`Temp: ${this.temp}°F`);
+    let currentWind = $('<p>').addClass('current-wind').text(`Wind: ${this.wind} mph`);
+    let currentHumidity = $('<p>').addClass('current-humidity').text(`Humidity: ${this.humidity}%`);
+    let currentUvi = $('<p>').addClass('current-uvi').text('UV Index: ');
+    let currentUviSpan = $('<span>').text(this.uvi).css({'color': 'white', 'background-color': this.uvi_color, 'padding': '3px 10px', 'border-radius': '5px'});
+    currentUvi.append(currentUviSpan);
+    currentDiv.append(currentDate);
+    currentDiv.append(currentIcon);
+    currentDiv.append(currentTemp);
+    currentDiv.append(currentWind);
+    currentDiv.append(currentHumidity);
+    currentDiv.append(currentUvi);
+    
+    return currentDiv;
+  }
+  getUviColor(uvi) {
+    if (uvi < 2) {
+      return "green";
+      
+    } else if (uvi > 2 && uvi < 6) {
+      return "yellow";
+    
+    } else if (uvi > 6 && uvi < 8) {
+      return "orange";
+    
+    } else if (uvi > 7) {
+      return "red";
+    }
+  }
+}
+
+class ForecastCard {
+    constructor(cardName, daily, index) {
+      this.name = cardName;
+      this.date = daily[index].dt;
+      this.icon = daily[index].weather[0].icon;
+      this.temp = daily[index].temp.max;
+      this.wind = daily[index].wind_speed;
+      this.humidity = daily[index].humidity;
+    }
+
+    createCard() {
+        let date = moment(this.date * 1000).format('MM/DD/YY');
+        let icon = `http://openweathermap.org/img/wn/${this.icon}@4x.png`
+        let cardDiv = $('<div>').addClass('h-100 forecast-card-1 card m-2 border border-1 border-dark').attr('style', "width: 10rem;");
+        let cardDate = $('<h3>').addClass('forecast-date').text(date);
+        let cardIcon = $('<img>').addClass('card-weather-icon').attr({'src': icon, 'alt': 'Weather Icon'});
+        let cardTemp = $('<p>').addClass('card-temp').text(`Temp: ${this.temp}°F`);
+        let cardWind = $('<p>').addClass('card-wind').text(`Wind: ${this.wind} mph`);
+        let cardHumidity = $('<p>').addClass('card-humidity').text(`Humidity: ${this.humidity}%`);
+        cardDiv.append(cardDate);
+        cardDiv.append(cardIcon);
+        cardDiv.append(cardTemp);
+        cardDiv.append(cardWind);
+        cardDiv.append(cardHumidity);
+        
+        return cardDiv;
+      }
+    }
+
+    
